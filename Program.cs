54e3d19bd876a2f7e09279a879203c013046b0a1
Armageddon47 <123872869+Armageddon47@users.noreply.github.com>
2023-02-13 20:12:00 +0400

    modify Rooms

--- Program.cs
+++ Program.cs
@@ -1,275 +1,118 @@
 ﻿using System.IO;
+using System.Runtime.InteropServices;
 
-enum RoomStates
-    {
-    Hall1,
-    Room1,
-    Room2
-}
-interface IRoom
-{
-    public string Name { get; set; }
-    public string Description { get; set; }
-    public void Render();
-
-}
 interface IState
 {
-    void Render();
-    ICommand GetCommand();
+
 }
 interface ICommand
 {
-    void Execute();
+    void GetCommand();
 }
 
-//class Room
-//{
-//    public string Name { get; set; }
-//    public string Description { get; set; }
-//    public Room(string name, string description)
-//    {
-//        Name = name;
-//        Description = description;
-//    }
-//}
-class Hall : IRoom
+interface IGameObject
 {
-    public string Name { get; set; }
-    public string Description { get; set; }
-
-    public Hall()
-    {
-
-        Name = "hall";
-        Description = "halldesx";
-    }
-    public void Render()
-    {
-        Console.WriteLine("you are now in {0} - {1}",Name,Description);
-    }
-}
-    class Room1 : IRoom
-    {
-        public string Name { get; set; }
-        public string Description { get; set; }
-
-        public Room1()
-        {
-
-            Name = "Room1";
-            Description = "room1desc";
-        }
-    public void Render()
-    {
-        Console.WriteLine("you are now in {0} - {1}", Name, Description);
-    }
-}
-
-class Room2 : IRoom
-{
-        public string Name { get; set; }
-        public string Description { get; set; }
-
-        public Room2()
-        {
-
-            Name = "Room2";
-            Description = "room2desc";
-        }
-    public void Render()
-    {
-        Console.WriteLine("you are now in {0} - {1}", Name, Description);
-    }
-}
-class Player
-{
-    public string Name { get; private set; }
-    public RoomStates State { get; set; }
-    = RoomStates.Hall1;
-    public Player()
-    {
-        Player player = new Player();
-        while (true)
-        {
-            Console.WriteLine("Enter Player Name");
-            string name = Console.ReadLine();
-            if (name != "")
-            {
-                Name = name; 
-                PlayerStats playerstats = new(player);
-                break;
-            }
-            
-        }
-        
-    }
-    public RoomStates GetState()
-    {
-        return State;
-    }
-    public void SetState(RoomStates st)
-    {
-        
-    }
-}
-//class SaveGame
-//{
-//    public SaveGame(RoomState roomstate)
-//    {
-
-//    }
-//}
-class LoadGame
-{
-   public void Render()
-    {
-
-    }
-    public ICommand GetCommand()
-    {
-        return null;
-    }
-}
-class NewGameCommand : ICommand
-{
-    public void Execute()
-    {
-        Player player = new Player();
-        GameStatsEngine gameStatsEngine = new GameStatsEngine();
-        
-        Game game = new Game();
-        game.Run(player,gameStatsEngine);
-        
-    }
-}
-class LoadCommand : ICommand
-{
-    public void Execute()
-    {
-        LoadGame load = new LoadGame();
-    }
-}
-class InvalidCommand : ICommand
-{
-    public void Execute()
-    {
-        Console.WriteLine("Invalid Command");
-    }
-}
-class GameStatsEngine
-{
-    private RoomStates _states;
-    Player player = new Player(); 
-    public GameStatsEngine()
-    {
-        RoomStates states = new RoomStates();
-        states = RoomStates.Hall1;
-        _states = states;
-    }
-    public IRoom GetRoomState()
-    {
-        player.GetState();
-        RoomStates states = player.State;
-        
-        if (states == RoomStates.Hall1)
-        {
-            Hall hall1 = new Hall();
-            hall1.Render();
-           
-            return hall1;
-        }
-        else if (states == RoomStates.Room1)
-        {
-            Room1 room1 = new Room1();
-            room1.Render();
-            return room1;
-        }
-        else if (states == RoomStates.Room2)
-        {
-            Room2 room2 = new Room2();
-            room2.Render();
-            return room2;
-        }
-        Hall noselection = new Hall();
-        noselection.Render();
-        return noselection;
-    }
-
-}
-//class PlayerStats
-//{
     
-//    public Player Player { get; set; }
-//    public  PlayerStats(Player player)
-//    {
-//        Player = player;
-//    }
-//    public Player GetPlayerStats()
-//    {
-//        return Player;
-//    }
-//    public void SetPlayerStats(RoomStates stats)
-//    {
-//        Player.State = stats;
-//    }
-
-//}
-class MainManuState : IState
-{
-    public void Render()
-    {
-        Console.WriteLine("Main Manu");
-        Console.WriteLine("[new] For New Game");
-        Console.WriteLine("[load] For Game Load");
-
-    }
-    public ICommand GetCommand()
-    {
-        var command = Console.ReadLine();
-        if (command == "new")
-        {
-            return new NewGameCommand();
-        }
-        else if (command == "load")
-        {
-            return new LoadCommand();
-        }
-        else
-        {
-            return new InvalidCommand();
-        }
-    }
 }
-class Game
-{
-    public Player Player { get; set; }
-    public IRoom Room { get; set; }
-    public GameStatsEngine GameStateEngine { get; set; }
-    public void Run(Player player,  GameStatsEngine gamestateengine)
-    {
 
-        Player = player;
-        
-        GameStateEngine = gamestateengine;
 
-        
-    }
 
-}
 class Program
 {
     static void Main()
     {
-        GameStatsEngine gamestats = new GameStatsEngine();
-        MainManuState mainManuState = new MainManuState();
-        while (true)
-        {
-            mainManuState.Render();
-            var command = mainManuState.GetCommand();
-            command.Execute();
+        //GameStatsEngine gamestats = new GameStatsEngine();
+        //MainManuState mainManuState = new MainManuState();
+        //while (true)
+        //{
+        //    mainManuState.Render();
+        //    var command = mainManuState.GetCommand();
+        //    command.Execute();
 
+        //}
+
+    }
+
+}
+
+class Player
+{
+
+}
+class Room
+{
+    private List<IGameObject> _inventory = new List<IGameObject>();
+    private string _name;
+    public Room( string name)
+    {
+        
+        _name = name;
+    }
+    public List<Room> RoomsList()
+    {
+        List<Room> rooms = new List<Room>();
+        var Hall = new Room("Hall");
+        var Room1 = new Room("Room 1");
+        var Room2 = new Room("Room2");
+        rooms.Add(Hall);
+        rooms.Add(Room1);
+        rooms.Add(Room2);
+        return rooms;
+
+    }
+    IGameObject GetItems(Room room)
+    {
+        List<IGameObject> item = new List<IGameObject>();
+        if (room._inventory.Count > 0)
+        {
+            for (int i = 0; i < room._inventory.Count; i++)
+            {
+                Console.WriteLine((i + 1) + ": " +room._inventory[i]);
+            }
         }
+        else
+        {
+            Console.WriteLine("Empty");
+        }
+        if (room._inventory.Count > 0)
+        {
+            int temp = 0;
+            bool guardian = true;
+            Console.WriteLine("Enter the number you want to take.");
+            var input = Console.ReadLine();
+            guardian = int.TryParse(input, out temp);
+            temp -= temp;
+            if (!guardian && room._inventory[temp]!= null)
+            {
+                item.Add(room._inventory[temp]);
+                room._inventory.RemoveAt(temp);
+            }
+            else
+            {
+                Console.WriteLine("INVALID");
+            }
+        }
+
+        return item[0];
+    }
+    Room DropItems(Room room ,IGameObject temp)
+    {
+        room._inventory.Add(temp);
+        return room;
+    }
+
+}
+
+class GameObjects : IGameObject
+{
+
+}
+
+class GameEngine
+{
+    private Player player = new Player();
+    public GameEngine()
+    {
 
     }
 }
